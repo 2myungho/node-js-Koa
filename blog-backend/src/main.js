9a199efad164c8c3de354af37639b729fdf1403a
lee myung ho <dl_audgh@naver.com>
2020-06-20 00:06:28 +0900

    오류 수정

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -4,37 +4,34 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
-import api from './api'
+import api from './api';
 
-//비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const {PORT,MONGO_URI} = process.env;
-
+// 비구조화 할당을 통하여 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URI } = process.env;
 
 mongoose
-.connect(MONGO_URI, {useNewUrlParser: true, useFindAndModify:false})
-    .then(() => {
-        console.log('Connected to MongoDB')
-    })
-    .catch(e => {
-        console.error(e)
-    })
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.error(e);
+  });
 
 const app = new Koa();
-const router= new Router();
+const router = new Router();
 
-//라우터 설정
-router.use('/api',api.routes()) //api 라우트 적용
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
 
-//라우터 적용 전에 bodyParser 적용
-app.use(bodyParser())
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
-//app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods())
-
-//post가 지정되어 있지 않다면 4000을 사용
+// PORT 가 지정되어있지 않다면 4000 을 사용
 const port = PORT || 4000;
 app.listen(port, () => {
-    console.log('Listening to port %d',port)
-})
-//서버를 포트 4000번으로 열고 서버에 접속하면 'hello world'라는 텍스트를 반환
+  console.log('Listening to port %d', port);
+});
